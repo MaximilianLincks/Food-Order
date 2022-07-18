42622c3508273b6ee22df7f62ae3e6c0c76c1f05
mli <maximilian.lincks@dscsag.com>
2022-07-18 08:16:07 +0200

    added dynmaic cont to HeaderButton

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -2,15 +2,23 @@ import "../cart/CartIcon";
 import CartIcon from "../cart/CartIcon";
 import { headerProps } from "./Header";
 import classes from "./styles/HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context"
+import { useContext } from "react";
 
 const HeaderCartButton = (props: headerProps) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items
+    .map(item => item.amount)
+    .reduce((acc, x) => {return acc + x}, 0);
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.icon}>3</span>
+      <span className={classes.icon}>{numberOfCartItems}</span>
     </button>
   );
 };
